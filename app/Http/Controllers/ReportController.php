f56506c1ec5262afbca8f9017d7a67679e7ce48e
Atefe <aa.asgariyan12@gmail.com>
2024-03-11 10:54:54 +0330

    create report creation endpoint

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\JsonHelper;
+use App\Http\Requests\report\StoreReportRequest;
 use App\Http\Resources\ReportTemplateResource;
+use App\Models\Report;
 use App\Models\ReportTemplate;
+use App\Models\Test;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class ReportController extends Controller
 {
@@ -22,7 +29,33 @@ class ReportController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreReportRequest $request)
+    {
+        $user = Auth::user();
+        $testId = $request->testId;
+        $test = Test::findOrFail($testId);
+        $this->authorize('update', $test);
+
+        try {
+            Report::create([
+                'test_id' => $testId,
+                'user_id' => $user->id,
+                'data' => JsonHelper::encodeJson($request->sections)
+            ]);
+            $test->update([
+                'status' => 'answered'
+            ]);
+            return response()->json(['data' => 'success']);
+        } catch (Exception $e) {
+            Log::info('Something went wrong creating a report: ' . $e->getMessage());
+            return response()->json(['errors' => 'Something went wrong creating a report.'], 500);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
     {
         //
     }
@@ -31,14 +64,6 @@ class ReportController extends Controller
      * Display the specified resource.
      */
     public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
     {
         //
     }
---
+++ app/Http/Requests/report/StoreReportRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\report;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreReportRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|integer',
+            'testId' => 'required|integer|exists:tests,id',
+            'testTitle' => 'required|string',
+            'sections' => 'required|array',
+            'sections.*.id' => 'required|integer',
+            'sections.*.sectionTitle' => 'required|string',
+            'sections.*.groups' => 'required|array',
+            'sections.*.groups.*.title' => 'nullable|string',
+            'sections.*.groups.*.className' => 'nullable|string',
+            'sections.*.groups.*.options' => 'required|array',
+            'sections.*.groups.*.options.*.id' => 'required|integer',
+            'sections.*.groups.*.options.*.type' => 'required|string',
+            'sections.*.groups.*.options.*.value' => 'required',
+            'sections.*.groups.*.options.*.label' => 'nullable|string',
+            'sections.*.groups.*.options.*.className' => 'nullable|string',
+        ];
+    }
+}
